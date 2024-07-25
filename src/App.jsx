2a1c140e4bf36616dd9397f3a72c1fa7652490c9
Maxim <maxim.sukhanov@sibers.com>
2024-07-25 11:52:50 +0600

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
-const App = () => {
-  return <div>HOME PAGE</div>;
-};
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>Test Application</div>
+    </>
+  );
+}
 
 export default App;
