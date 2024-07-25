e6c47df2ac6d994f79c68781f4087bdf6e277bbb
Maxim <maxim.sukhanov@sibers.com>
2024-07-25 11:31:24 +0600

    init application

---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Link, RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        <h1>Hello World</h1>
+        <Link to="about">About Us</Link>
+      </div>
+    ),
+  },
+  {
+    path: "about",
+    element: (
+      <div>
+        <h1>About</h1>
+        <Link to="/">Go back to home</Link>
+      </div>
+    ),
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router}>
+    <App />
+  </RouterProvider>
+);
